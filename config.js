88ffc77be9f6c5e62142639f9646a8de555cb6c8
mohammadreadlewe98-ship-it <mohammadreadlewe.98@gmail.com>
2026-05-14 16:25:36 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-	// config.js
+// config.js
 const CONFIG = {
   API_URL: 'https://script.google.com/macros/s/AKfycbwRX3-G0t_0B83Vg9vBy3X7nPUUgfAdLJrSaz-lon1oQWBazCG_Xg3pFS-POt6LrCQ6Ew/exec',
   REGIONS: ['دمشق', 'ريف دمشق', 'حمص', 'حماة', 'دير الزور', 'إدلب', 'درعا'],
