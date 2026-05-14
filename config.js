23da76cbe247fb0d27826584ad84ec229292136f
mohammadreadlewe98-ship-it <mohammadreadlewe.98@gmail.com>
2026-05-14 18:28:28 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 const CONFIG = {
-  API_URL: 'https://script.google.com/macros/s/AKfycbwRX3-G0t_0B83Vg9vBy3X7nPUUgfAdLJrSaz-lon1oQWBazCG_Xg3pFS-POt6LrCQ6Ew/exec',
+  API_URL: '/api/proxy',
   REGIONS: ['دمشق', 'ريف دمشق', 'حمص', 'حماة', 'دير الزور', 'إدلب', 'درعا'],
   SITE_NAME: 'مؤسسة النبأ العظيم',
   SITE_SUBTITLE: 'نظام إدارة البيانات التعليمية',
